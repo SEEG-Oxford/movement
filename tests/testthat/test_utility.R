3f5d9cda14a90227281ee54b4be76047ff76a0d1
Andrew Schofield <andrew.schofield@tessella.com>
2015-03-05 12:01:18 +0000

    Added 3 tests for correlateregions

--- tests/testthat/test_utility.R
+++ tests/testthat/test_utility.R
@@ -59,3 +59,23 @@ test_that("as.movementmatrix returns the correct matrix", {
 	expectedmatrix <- matrix(c(0,1,2,0),nrow=2,dimnames=list(c("a","b"),c("a","b")))
 	expect_equal(as.movementmatrix(testdata), expectedmatrix)
 })
+
+test_that("correlate regions works for small test case", {
+	testdataframe <- data.frame(name=c("a", "b", "c", "d"), location=c(1,2,3,4), pop=c(10,20,30,40), lon=c(-5,-4,-3,-2), lat=c(-1,0,1,2))
+	testregionlist <- data.frame(V1=c(1,2,3,4),V2=c("a","b","c","d"))
+	testmovementdata <- data.frame(origin=c(1,1,1,2,2,2,3,3,3,4,4,4), destination=c(2,3,4,1,3,4,1,2,4,1,2,3), movement=c(1,2,3,4,5,6,7,8,9,10,11,12))
+	actual <- correlateregions(testdataframe, testregionlist, testmovementdata)
+	expectedlocations <- data.frame(name=c("a", "b", "c", "d"), pop=c(10,20,30,40), lon=c(-5,-4,-3,-2), lat=c(-1,0,1,2))
+	expectedobserved <- matrix(c(0,1,2,3,4,5,0,6,7,8,9,0,10,11,12,0), nrow=4, dimnames=list(c("a", "b", "c", "d"),c("a", "b", "c", "d")))
+	expect_equal(actual$locations, expectedlocations)
+})
+
+test_that("correlate regions removes locations from dataframe when not present as movement data", {
+	testdataframe <- data.frame(name=c("a", "b", "c", "d", "e"), location=c(1,2,3,4,5), pop=c(10,20,30,40,50), lon=c(-5,-4,-3,-2,-1), lat=c(-1,0,1,2,3))
+	testregionlist <- data.frame(V1=c(1,2,3,4),V2=c("a","b","c","d"))
+	testmovementdata <- data.frame(origin=c(1,1,1,2,2,2,3,3,3,4,4,4), destination=c(2,3,4,1,3,4,1,2,4,1,2,3), movement=c(1,2,3,4,5,6,7,8,9,10,11,12))
+	actual <- correlateregions(testdataframe, testregionlist, testmovementdata)
+	expectedlocations <- data.frame(name=c("a", "b", "c", "d"), pop=c(10,20,30,40), lon=c(-5,-4,-3,-2), lat=c(-1,0,1,2))
+	expectedobserved <- matrix(c(0,1,2,3,4,5,0,6,7,8,9,0,10,11,12,0), nrow=4, dimnames=list(c("a", "b", "c", "d"),c("a", "b", "c", "d")))
+	expect_equal(actual$locations, expectedlocations)
+})
