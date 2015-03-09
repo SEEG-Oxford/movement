a402a67d054a463db99d9ca2417c489e9406d854
Andrew Schofield <andrew.schofield@tessella.com>
2015-03-09 14:25:01 +0000

    Removed commented out test with offending UTF-8 character

--- tests/testthat/test_utility.R
+++ tests/testthat/test_utility.R
@@ -9,11 +9,6 @@ test_that("spaces are replaced with underscores", {
 	expect_equal(simplifytext("string with spaces"), "STRING_WITH_SPACES")
 })
 
-# not sure why this one fails as it appears to work correctly in code
-test_that("non ascii characters are converted", {
-	expect_equal(simplifytext("Ö"), "O")
-})
-
 test_that("as.locationdataframe removes duplicate origins", {
 	testdata <- data.frame(origin=c(1,1,2,3,4), pop_origin=c(10,10,20,30,40), lat_origin=c(-1,-1,0,1,2), long_origin=c(-5,-5,-4,-3,-2))
 	expect_equal(nrow(as.locationdataframe(testdata)), 4)
