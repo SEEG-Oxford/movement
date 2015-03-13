414b39440593b09c3cfc35f5cfa8135d80f0fd6c
Andrew Schofield <andrew.schofield@tessella.com>
2015-03-13 11:10:47 +0000

    Add more comprehensive tests for gravity.flux()

--- tests/testthat/test_model_prediction.R
+++ tests/testthat/test_model_prediction.R
@@ -8,3 +8,27 @@ test_that("gravity.flux gives expected results with default parameters", {
 	population <- c(1000,2000)
 	expect_equal(gravity.flux(i, j, distance, population), c(617.0338627200094379077,759.6577929323736952938))
 })
+
+test_that("gravity.flux gives expected results with symmetric = TRUE", {
+	i <- 1
+	j <- 2
+	distance <- matrix(c(0,1,1,0),nrow=2)
+	population <- c(1000,2000)
+	expect_equal(gravity.flux(i, j, distance, population, symmetric=TRUE), 1376.6916556523831332015)
+})
+
+test_that("gravity.flux gives expected results with minpop set", {
+	i <- 1
+	j <- 2
+	distance <- matrix(c(0,1,1,0),nrow=2)
+	population <- c(1000,2000)
+	expect_equal(gravity.flux(i, j, distance, population, symmetric=TRUE, minpop=1001), 0)
+})
+
+test_that("gravity.flux gives expected results with maxrange set", {
+	i <- 1
+	j <- 2
+	distance <- matrix(c(0,1,1,0),nrow=2)
+	population <- c(1000,2000)
+	expect_equal(gravity.flux(i, j, distance, population, symmetric=TRUE, maxrange=0.1), Inf)
+})
