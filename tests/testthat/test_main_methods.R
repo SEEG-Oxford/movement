3fa7032e7630900adf839a046b64d62c9acd7d60
Kathrin Wuensch <kathrin.wuensch@tessella.com>
2015-10-16 09:21:53 +0100

    Issue-35: fixed the first unit tests for the predict.optimisedmodel Need to explicit specify the environment of the function which will be replaced by a mock implementation - in these cases it's the 'predict.optimsedmodel'. Here, add the environment with 'movement::predict.optimisedmodel' to replace the correct function within the mock environment.

--- tests/testthat/test_main_methods.R
+++ tests/testthat/test_main_methods.R
@@ -2,24 +2,26 @@ library(movement)
 library(raster)
 context("Main Interface Methods")
 
-# test_that("predict.optimisedmodel returns list of correct data when given a RasterLayer", {
-# 	predictionModel <- list(trainingresults=NULL)
-# 	dataframe <- raster::raster(nrows=108, ncols=21, xmn=0, xmx=10)
-# 	with_mock(predict.movementmodel = function(x) {return (list(net=list(locations=1,population=1,coordinates=1),prediction=2))},
-# 		expect_equal(predict.optimisedmodel(predictionModel,dataframe),list(df_locations=data.frame(location=1,pop=1,coordinates=1),movement_matrix=2))
-# 	)
-# })
+test_that("predict.optimisedmodel returns list of correct data when given a RasterLayer", {
+	predictionModel <- list(trainingresults=NULL)
+	dataframe <- raster::raster(nrows=108, ncols=21, xmn=0, xmx=10)
+	with_mock(`movement::predict.movementmodel` = function(x) {return (list(net=list(locations=1,population=1,coordinates=1),prediction=2))},
+            expect_equal(predict.optimisedmodel(predictionModel,dataframe),list(df_locations=data.frame(location=1,pop=1,coordinates=1),movement_matrix=2))
+	)
+})
 
-# test_that("predict.optimisedmodel returns list of correct data when given a data.frame", {
-# 	predictionModel <- list(trainingresults=NULL)
-# 	dataframe <- data.frame(c(1))
-#   print(str(predictionModel))
-#   print(dataframe)
-# 	with_mock(predict.movement = function(x, y) {return (list(net=list(locations=1,population=1,coordinates=1),prediction=2))},
-# 		expect_equal(predict.optimisedmodel(predictionModel,dataframe),list(df_locations=data.frame(location=1,pop=1,coordinates=1),movement_matrix=2))
-# 	)
-# })
-
+test_that("predict.optimisedmodel returns list of correct data when given a data.frame", {
+  predictionModel <- list(trainingresults=NULL)
+  class(predictionModel) <- 'optimisedmodel'
+  dataframe <- data.frame(c(1))    
+  with_mock(# note: need to specify explicit the environment of the function which will be replaced by a mock implementation
+            `movement::predict.movementmodel` = function(x,...) {return (list(net=list(locations=1,population=1,coordinates=1),prediction=2))},
+            expected_list <- list(df_locations=data.frame(location=1,pop=1,coordinates=1), movement_matrix = 2),
+            actual_predict_optimisedmodel  <- predict.optimisedmodel(predictionModel,dataframe), 
+            expect_equal(actual_predict_optimisedmodel, expected_list)
+  )  
+})
+  
 test_that("predict.optimisedmodel throws an error if given the wrong type", {
 	predictionModel <- list(trainingresults=NULL)
 	class(predictionModel) <- 'optimisedmodel'
